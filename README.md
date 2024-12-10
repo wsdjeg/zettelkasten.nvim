020a35ac80a81bd9e15da05efbb9a4c7cdce45cb
Eric Wong <eric@wsdjeg.net>
2024-12-10 14:37:01 +0000

    feat(zettelkasten): detach vim-zettelkasten plugin
    update
    
    zettelkasten browser statusline
    
    update
    
    update readme
    
    update

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # vim-zettelkasten
+
+> _vim-zettelkasten_ is a [Zettelkasten](https://zettelkasten.de) note taking plugin, which is forked from [zettelkasten.nvim@fe174666](https://github.com/Furkanzmc/zettelkasten.nvim/tree/fe1746666e27c2fcc0e60dc2786cb9983b994759).
+
+[![](https://spacevim.org/img/build-with-SpaceVim.svg)](https://spacevim.org)
+[![GPLv3 License](https://img.spacevim.org/license-GPLv3-blue.svg)](LICENSE)
+
+<!-- vim-markdown-toc GFM -->
+
+- [Install](#install)
+- [Feedback](#feedback)
+
+<!-- vim-markdown-toc -->
+
+## Install
+
+1. Using `vim-zettelkasten` in SpaceVim:
+
+```toml
+[[layers]]
+  name = 'zettelkasten'
+  zettel_dir = 'D:\me\zettelkasten'
+  zettel_template_dir = 'D:\me\zettelkasten_template'
+```
+
+2. Using `vim-zettelkasten` without SpaceVim:
+
+```
+Plug 'wsdjeg/vim-zettelkasten'
+```
+
+## Feedback
+
+The development of this plugin is in [`SpaceVim/bundle/vim-zettelkasten`](https://github.com/SpaceVim/SpaceVim/tree/master/bundle/vim-zettelkasten) directory.
+
+If you encounter any bugs or have suggestions, please file an issue in the [issue tracker](https://github.com/SpaceVim/SpaceVim/issues)
---
+++ lua/zettelkasten/formatter.lua
@@ -0,0 +1,66 @@
+--=============================================================================
+-- formatter.lua --- formatter for zk notes browser
+-- Copyright (c) 2024 Wang Shidong & Contributors
+-- Author: Wang Shidong < wsdjeg@outlook.com >
+-- URL: https://spacevim.org
+-- License: GPLv3
+--=============================================================================
+
+local M = {}
+local s_formatters = {
+  ['%r'] = function(line)
+    return #line.references
+  end,
+  ['%b'] = function(line)
+    return #line.back_references
+  end,
+  ['%f'] = function(line)
+    return vim.fn.fnamemodify(line.file_name, ':t')
+  end,
+  ['%h'] = function(line)
+    if vim.fn.strdisplaywidth(line.title) <= 30 then
+      return line.title .. string.rep(' ', 30 - vim.fn.strdisplaywidth(line.title))
+    end
+    return line.title
+  end,
+  ['%d'] = function(line)
+    return line.id
+  end,
+  ['%t'] = function(line)
+    local tags = {}
+    for _, tag in ipairs(line.tags) do
+      if vim.tbl_contains(tags, tag.name) == false then
+        table.insert(tags, tag.name)
+      end
+    end
+
+    return table.concat(tags, ' ')
+  end,
+}
+
+local function get_format_keys(format)
+  local matches = {}
+  for w in string.gmatch(format, '%%%a') do
+    table.insert(matches, w)
+  end
+
+  return matches
+end
+
+function M.format(lines, format)
+  local formatted_lines = {}
+  local modifiers = get_format_keys(format)
+  for _, line in ipairs(lines) do
+    local cmps = format
+    for _, modifier in ipairs(modifiers) do
+      local rst = s_formatters[modifier](line) or ''
+      cmps = string.gsub(cmps, '%' .. modifier, rst)
+    end
+
+    table.insert(formatted_lines, cmps)
+  end
+
+  return formatted_lines
+end
+
+return M
