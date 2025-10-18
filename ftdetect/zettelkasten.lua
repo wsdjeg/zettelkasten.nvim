b7fcad9a8948ae10d109b1e88a5ecc6f5004327b
Eric Wong <wsdjeg@outlook.com>
2025-10-18 13:27:46 +0800

    fix: fix zettelkasten browser ftdetect

--- ftdetect/zettelkasten.lua
+++ ftdetect/zettelkasten.lua
@@ -2,7 +2,7 @@ local augroup = vim.api.nvim_create_augroup('zettelkasten_ft', {clear = true})
 
 vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
     group = augroup,
-    pattern = {'zk:browser'},
+    pattern = {'zk://browser'},
     callback = function(ev)
         vim.api.nvim_buf_set_option(ev.buf, 'filetype', 'zkbrowser')
     end
