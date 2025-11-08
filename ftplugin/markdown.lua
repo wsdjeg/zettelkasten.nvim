760f3540bb74aa183967f9522f79628f4afb2c74
Eric Wong <wsdjeg@outlook.com>
2025-11-08 22:17:42 +0800

    fix: only set zk ftplugin for zettelkasten file

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -1,34 +1,32 @@
-local zk = require("zettelkasten")
+local re = [[\d\d\d\d-\d\d-\d\d-\d\d-\d\d-\d\d\.md]]
 
-if vim.opt_local.tagfunc:get() == "" then
-    vim.opt_local.tagfunc = "v:lua.zettelkasten.tagfunc"
+if not vim.regex(re):match_str(vim.api.nvim_buf_get_name(0)) then
+    return
 end
 
-if vim.opt_local.completefunc:get() == "" then
-    vim.opt_local.completefunc = "v:lua.zettelkasten.completefunc"
-end
+vim.opt_local.tagfunc = 'v:lua.zettelkasten.tagfunc'
 
-vim.opt.isfname:append(":")
-vim.opt.isfname:append("-")
-vim.opt_local.iskeyword:append(":")
-vim.opt_local.iskeyword:append("-")
-vim.opt_local.suffixesadd:append(".md")
-vim.opt_local.errorformat = "%f:%l: %m"
-vim.opt_local.include = "[[\\s]]"
-vim.opt_local.define = "^# \\s*"
+vim.opt_local.completefunc = 'v:lua.zettelkasten.completefunc'
 
-if vim.opt_local.keywordprg:get() == "" then
-    vim.opt_local.keywordprg = ":ZkHover"
-end
+vim.opt.isfname:append(':')
+vim.opt.isfname:append('-')
+vim.opt_local.iskeyword:append(':')
+vim.opt_local.iskeyword:append('-')
+vim.opt_local.suffixesadd:append('.md')
+vim.opt_local.errorformat = '%f:%l: %m'
+vim.opt_local.include = '[[\\s]]'
+vim.opt_local.define = '^# \\s*'
 
-if vim.fn.mapcheck("[I", "n") == "" then
+vim.opt_local.keywordprg = ':ZkHover'
+
+if vim.fn.mapcheck('[I', 'n') == '' then
     vim.api.nvim_buf_set_keymap(
         0,
-        "n",
-        "[I",
+        'n',
+        '[I',
         '<CMD>lua require("zettelkasten").show_back_references(vim.fn.expand("<cword>"))<CR>',
         { noremap = true, silent = true, nowait = true }
     )
 end
 
-require("zettelkasten").add_hover_command()
+require('zettelkasten').add_hover_command()
