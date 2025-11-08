0342f65337e270ebfdb0de29471f185ee51cea7f
Eric Wong <wsdjeg@outlook.com>
2025-11-08 23:36:53 +0800

    fix: fix setup function

--- lua/zettelkasten/config.lua
+++ lua/zettelkasten/config.lua
@@ -8,22 +8,29 @@
 local M = {}
 
 if vim.g.zettelkasten_directory and vim.g.zettelkasten_directory ~= '' then
-  M.zettel_dir = vim.g.zettelkasten_directory
+    M.zettel_dir = vim.g.zettelkasten_directory
 else
-  M.zettel_dir = '~/.zettelkasten/'
+    M.zettel_dir = '~/.zettelkasten/'
 end
 
 if vim.g.zettelkasten_template_directory and vim.g.zettelkasten_template_directory ~= '' then
-  M.templete_dir = vim.g.zettelkasten_template_directory
+    M.templete_dir = vim.g.zettelkasten_template_directory
 else
-  M.templete_dir = '~/.zettelkasten_template'
+    M.templete_dir = '~/.zettelkasten_template'
 end
 
 M.browseformat = '%f - %h [%r Refs] [%b B-Refs] %t'
 M.preview_command = 'pedit'
 
 M.get = function()
-  return nil
+    return nil
+end
+
+M._set = function(opts)
+    opts = opts or {}
+    M.zettel_dir = opts.notes_path or M.zettel_dir
+    M.preview_command = opts.preview_command or M.preview_command
+    M.browseformat = opts.browseformat or M.browseformat
 end
 
 return M
